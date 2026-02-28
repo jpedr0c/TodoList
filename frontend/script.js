a28acabc1e77dcbe7e8a4e3dd395aafa371dfb11
jpedr0c <jpccarvalho2210@gmail.com>
2026-02-28 11:55:06 -0300

    feat: Finalizado abertura e fechamento da modal de Nova Tarefa

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,40 @@
 lucide.createIcons();
+
+let btnNewTask = document.getElementById("btnNewTask");
+let btnCancel = document.getElementById("btnCancel");
+let btnCloseModal = document.getElementById("btnCloseModal");
+let modalTitle = document.getElementById("modalTitle");
+let taskModal = document.getElementById('taskModal');
+let taskForm = document.getElementById('taskForm');
+let taskTitle = document.getElementById('taskTitle');
+let taskDescription = document.getElementById('taskDescription');
+let taskStatus = document.getElementById('taskStatus');
+let taskPriority = document.getElementById('taskPriority');
+
+
+btnNewTask.addEventListener("click", () => openNewTaskModal());
+btnCancel.addEventListener("click", () => closeModal());
+btnCloseModal.addEventListener("click", () => closeModal());
+
+function closeModal() {
+    taskModal.classList.remove('show');
+    taskForm.reset();
+}
+
+function openNewTaskModal() {
+    modalTitle.textContent = 'Nova Tarefa';
+    taskForm.reset();
+
+    if (taskStatus) {
+        taskStatus.value = 'TODO';
+    }
+    if (taskPriority) {
+        taskPriority.value = 'MEDIA';
+    }
+
+    taskModal.classList.add('show');
+
+    if (taskTitle){
+        taskTitle.focus();
+    }
+}
